929a7cceeb860c6b45fd7eaf0ca274fccec44e3f
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-12-24 17:41:26 +0300

    Add client class with shared http client for each model

---
+++ src/ChadClient.cs
@@ -0,0 +1,37 @@
+﻿namespace Redplcs.ChadApi;
+
+public class ChadClient : IDisposable, IChadClient
+{
+	private readonly HttpClient _httpClient;
+	private bool _disposed;
+
+	public ChadClient()
+	{
+		_httpClient = new HttpClient();
+	}
+
+	public IChadGpt4oMiniModel Gpt4oMini { get; }
+	public IChadGpt4oModel Gpt4o { get; }
+	public IChadClaude3HaikuModel Claude3Haiku { get; }
+	public IChadClaude3OpusModel Claude3Opus { get; }
+	public IChadClaude3p5SonnetModel Claude3p5Sonnet { get; }
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!_disposed)
+		{
+			if (disposing)
+			{
+				_httpClient.Dispose();
+			}
+
+			_disposed = true;
+		}
+	}
+
+	public void Dispose()
+	{
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+}
